1fd4c9dd677e8b1260bcebaf3812c32725ca7c6b
Abdelkader Tehar <a.tehar19@gmail.com>
2025-03-11 17:14:09 +0100

    Script sql + vitefait DbSql

--- src/main/java/DataBase/DbSql.java
+++ src/main/java/DataBase/DbSql.java
@@ -1,4 +1,8 @@
 package DataBase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbSql {
 }
---
+++ src/main/java/DataBase/script.sql
@@ -0,0 +1,47 @@
+
+CREATE DATABASE IF NOT EXISTS DB_Heptathlon;
+
+USE DB_Heptathlon;
+
+--  Table Catégorie
+CREATE TABLE Categorie (
+    id_categorie INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nom_categorie NVARCHAR(250) NOT NULL,
+    description_categorie NVARCHAR(250)
+);
+
+-- Table Produit
+CREATE TABLE Produit (
+    id_produit INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_categorie INT NOT NULL,
+    nom_produit NVARCHAR(250) NOT NULL,
+    description_produit NVARCHAR(250),
+    prix_produit INT NOT NULL,
+    quantite_disponible INT NOT NULL,
+    FOREIGN KEY (id_categorie) REFERENCES Categorie(id_categorie)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+-- Table Commande
+CREATE TABLE Commande (
+    id_commande INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    date_commande DATE NOT NULL,
+    status_commande ENUM('En cours', 'Payé', 'Annulé') NOT NULL,
+    total_commande INT NOT NULL,
+    mode_de_paiement ENUM('Carte', 'Espèce', 'Virement') NOT NULL
+);
+
+-- Table DétailCommande
+CREATE TABLE DetailCommande (
+    id_detail INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_commande INT NOT NULL,
+    id_produit INT NOT NULL,
+    quantite_produit INT NOT NULL CHECK (quantite_produit > 0),
+    FOREIGN KEY (id_commande) REFERENCES Commande(id_commande)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    FOREIGN KEY (id_produit) REFERENCES Produit(id_produit)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
